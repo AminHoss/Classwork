8c746206330b6bfadf82375c436f4ec16ff3ed51
AminHoss <47458405+AminHoss@users.noreply.github.com>
2019-02-19 12:45:31 -0500

    Create Exercise3.py

---
+++ Exercise3.py
@@ -0,0 +1,4 @@
+width = float(input("What's the width of the room, in meters?  "))
+length = float(input("What's the length of the room, in meters?  "))
+area = length * width   
+print("The area of the room is", area, "meter(s)")
