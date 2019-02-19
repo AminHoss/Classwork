643ec551214b5943d0891e5cbd5477234b1dcd54
AminHoss <47458405+AminHoss@users.noreply.github.com>
2019-02-19 12:46:43 -0500

    Create Exercise4.py

---
+++ Exercise4.py
@@ -0,0 +1,5 @@
+width = int(input("How wide is the field, in feet?  "))
+length = int(input("How long is the field, in feet?  "))
+area = length * width
+acres = area/43560
+print(f"The amount of acres is {acres}")
