ff0f48afda3f17d6709999eb11c4e802bdb86d7d
AminHoss <47458405+AminHoss@users.noreply.github.com>
2019-02-19 12:43:37 -0500

    Create Exercise1.py

---
+++ Exercise1.py
@@ -0,0 +1,11 @@
+name = "Amin"
+address = "8101 Leslie St"
+city = "Thornhill"
+province = "ON"
+postal_code = "L3T 7P4"
+phone_number = "905-889-4982"
+print(name)
+print(address)
+print(city + ",", province)
+print(postal_code)
+print(phone_number)
